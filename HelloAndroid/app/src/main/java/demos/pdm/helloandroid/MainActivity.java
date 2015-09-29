a025a947437b026ec87deea4c5346b3e5c9801f5
Paulo Pereira <ppe04@ostml0195424.local>
2015-09-29 17:06:29 +0100

    Modified project structure to contain stable demo versions

--- HelloAndroid/app/src/main/java/demos/pdm/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/demos/pdm/helloandroid/MainActivity.java
@@ -10,51 +10,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int counterValue = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView msgBox = (TextView) findViewById(R.id.msg_box);
 
-        findViewById(R.id.dec_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_say).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View source) {
-                msgBox.setText(String.valueOf(--counterValue));
+            public void onClick(View v) {
+                findViewById(R.id.msg_box).setVisibility(View.VISIBLE);
             }
         });
-
-        findViewById(R.id.inc_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View source) {
-                msgBox.setText(String.valueOf(++counterValue));
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            TextView msgBox = (TextView) findViewById(R.id.msg_box);
-            msgBox.setText("UAU");
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
