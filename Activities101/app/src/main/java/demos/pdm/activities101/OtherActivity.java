a24e42f51bca52a82b78e3c1723212e674e41509
Paulo Pereira <ppe04@ostml0195424.bskyb.com>
2016-01-06 11:24:46 +0000

    Updated Activities101 demo in order to illustrate Activity lifecycle state transitions

--- Activities101/app/src/main/java/demos/pdm/activities101/OtherActivity.java
+++ Activities101/app/src/main/java/demos/pdm/activities101/OtherActivity.java
@@ -1,5 +1,9 @@
 package demos.pdm.activities101;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Another Activity used to illustrate user tasks and back-stack.
  *
@@ -9,4 +13,46 @@ package demos.pdm.activities101;
  */
 public class OtherActivity extends LifecycleActivityBase {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                Log.v("AsyncTask", "Starting...");
+                try { Thread.sleep(5000); }
+                catch (InterruptedException _) {}
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                if (OtherActivity.this.isFinishing()) {
+                    Log.v("AsyncTask", "onPostExecute is Finishing");
+                } else {
+                    Log.v("AsyncTask", "onPostExecute");
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try { Thread.sleep(3000); }
+        catch (InterruptedException _) {}
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try { Thread.sleep(4000); }
+        catch (InterruptedException _) {}
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
